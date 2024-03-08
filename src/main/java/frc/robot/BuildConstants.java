1765097e82f08f2964a779faee27e5d4db540d52
Marty Gubow <martygubow@gmail.com>
2024-03-07 19:01:58 -0500

    fix middle

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC2024B";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 22;
-  public static final String GIT_SHA = "efec8d7ae1fbe0ab0381f7fa0be89912a206a14d";
-  public static final String GIT_DATE = "2024-03-03 23:24:08 EST";
+  public static final int GIT_REVISION = 23;
+  public static final String GIT_SHA = "47e331ab0123e78f9e6c4429498a27061f62c663";
+  public static final String GIT_DATE = "2024-03-07 18:09:56 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2024-03-07 17:35:23 EST";
-  public static final long BUILD_UNIX_TIME = 1709850923486L;
+  public static final String BUILD_DATE = "2024-03-07 19:01:38 EST";
+  public static final long BUILD_UNIX_TIME = 1709856098965L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
