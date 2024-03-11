fea15848c9b5de43175e1953fe4a151aeebd4879
Marty Gubow <martygubow@gmail.com>
2024-03-11 07:58:25 -0400

    FIx start rotation

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC2024B";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 27;
-  public static final String GIT_SHA = "7c4e1f83942c0ecd3a2496a4e0a727e861fe88dd";
-  public static final String GIT_DATE = "2024-03-10 13:41:29 EDT";
+  public static final int GIT_REVISION = 28;
+  public static final String GIT_SHA = "dbcf27dd49588095351f21f3d450f3d498b749aa";
+  public static final String GIT_DATE = "2024-03-10 15:09:25 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2024-03-10 15:08:52 EDT";
-  public static final long BUILD_UNIX_TIME = 1710097732612L;
+  public static final String BUILD_DATE = "2024-03-11 07:57:21 EDT";
+  public static final long BUILD_UNIX_TIME = 1710158241367L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
